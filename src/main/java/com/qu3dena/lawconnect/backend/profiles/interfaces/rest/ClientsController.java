b0bfea27d4b6694a99cec99a729e4296331a6b1d
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-06-11 18:15:02 -0500

    feat(profiles): add resources, transforms and controllers.

---
+++ src/main/java/com/qu3dena/lawconnect/backend/profiles/interfaces/rest/ClientsController.java
@@ -0,0 +1,71 @@
+package com.qu3dena.lawconnect.backend.profiles.interfaces.rest;
+
+import com.qu3dena.lawconnect.backend.profiles.domain.model.queries.GetClientByDniQuery;
+import com.qu3dena.lawconnect.backend.profiles.domain.services.ClientCommandService;
+import com.qu3dena.lawconnect.backend.profiles.domain.services.ClientQueryService;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.resources.ClientResource;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.resources.CreateClientResource;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.transform.ClientResourceFromEntityAssembler;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.transform.CreateClientCommandFromResourceAssembler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/clients", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Profiles", description = "Profile Management Endpoints")
+public class ClientsController {
+
+    private final ClientCommandService commandService;
+    private final ClientQueryService queryService;
+
+    public ClientsController(ClientCommandService commandService, ClientQueryService queryService) {
+        this.commandService = commandService;
+        this.queryService = queryService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create client profile", description = "Creates a new client profile")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Client profile created"),
+            @ApiResponse(responseCode = "400", description = "Invalid request data"),
+    })
+    public ResponseEntity<ClientResource> createClientProfile(
+            @RequestBody CreateClientResource resource
+    ) {
+        var command = CreateClientCommandFromResourceAssembler.toCommandFromResource(resource);
+
+        var client = commandService.handle(command);
+
+        if (client.isEmpty())
+            return ResponseEntity.badRequest().build();
+
+        var clientResource = ClientResourceFromEntityAssembler.toResourceFromEntity(client.get());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientResource);
+    }
+
+    @GetMapping("{dni}")
+    @Operation(summary = "Get client profile by DNI", description = "Get client profile by DNI")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client profile found"),
+            @ApiResponse(responseCode = "404", description = "Client profile not found")
+    })
+    public ResponseEntity<ClientResource> getClientProfileByDni(
+            @PathVariable String dni
+    ) {
+        var query = new GetClientByDniQuery(dni);
+        var maybeItem = queryService.handle(query);
+
+        if (maybeItem.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        var resource = ClientResourceFromEntityAssembler.toResourceFromEntity(maybeItem.get());
+        return ResponseEntity.ok(resource);
+    }
+}
---
+++ src/main/java/com/qu3dena/lawconnect/backend/profiles/interfaces/rest/LawyersController.java
@@ -0,0 +1,71 @@
+package com.qu3dena.lawconnect.backend.profiles.interfaces.rest;
+
+import com.qu3dena.lawconnect.backend.profiles.domain.model.queries.GetLawyerByDniQuery;
+import com.qu3dena.lawconnect.backend.profiles.domain.services.LawyerCommandService;
+import com.qu3dena.lawconnect.backend.profiles.domain.services.LawyerQueryService;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.resources.CreateLawyerResource;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.resources.LawyerResource;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.transform.CreateLawyerCommandFromResourceAssembler;
+import com.qu3dena.lawconnect.backend.profiles.interfaces.rest.transform.LawyerResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/lawyers", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Profiles", description = "Profile Management Endpoints")
+public class LawyersController {
+
+    private final LawyerCommandService commandService;
+    private final LawyerQueryService queryService;
+
+    public LawyersController(LawyerCommandService commandService, LawyerQueryService queryService) {
+        this.commandService = commandService;
+        this.queryService = queryService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create lawyer profile", description = "Creates a new lawyer profile")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Lawyer profile created"),
+            @ApiResponse(responseCode = "400", description = "Invalid request data"),
+    })
+    public ResponseEntity<LawyerResource> createLawyerProfile(
+            @RequestBody CreateLawyerResource resource
+    ) {
+        var command = CreateLawyerCommandFromResourceAssembler.toCommandFromResource(resource);
+
+        var lawyer = commandService.handle(command);
+
+        if (lawyer.isEmpty())
+            return ResponseEntity.badRequest().build();
+
+        var lawyerResource = LawyerResourceFromEntityAssembler.toResourceFromEntity(lawyer.get());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lawyerResource);
+    }
+
+    @GetMapping("{dni}")
+    @Operation(summary = "Get lawyer profile by DNI", description = "Get lawyer profile by DNI")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lawyer profile found"),
+            @ApiResponse(responseCode = "404", description = "Lawyer profile not found")
+    })
+    public ResponseEntity<LawyerResource> getClientProfileByDni(
+            @PathVariable String dni
+    ) {
+        var query = new GetLawyerByDniQuery(dni);
+        var maybeItem = queryService.handle(query);
+
+        if (maybeItem.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        var resource = LawyerResourceFromEntityAssembler.toResourceFromEntity(maybeItem.get());
+        return ResponseEntity.ok(resource);
+    }
+}
